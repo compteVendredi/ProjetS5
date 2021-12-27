c8c852d9edcea283043191b47bebf4b0be5313da
Bastini <33761095+Bastini288@users.noreply.github.com>
2021-12-27 18:39:36 +0100

    Update BDD.java

--- src/serveur/BDD.java
+++ src/serveur/BDD.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import utilisateur.FilDiscussion;
 import utilisateur.Message;
@@ -65,7 +64,7 @@ public class BDD {
 	 * @param requete
 	 * @return resultatRequete ou null si erreur
 	 */
-	public ResultSet requeteLecture(String requete) {
+	private ResultSet requeteLecture(String requete) {
 		ResultSet resultSet = null;
 		try {
 			resultSet = stmt.executeQuery(requete);
@@ -83,7 +82,7 @@ public class BDD {
 	 * @param requete
 	 * @return 0 si succès 1 sinon
 	 */
-	public int requeteEcriture(String requete) {
+	private int requeteEcriture(String requete) {
 		try {
 			stmt.executeUpdate(requete);
 		} catch (SQLException e) {
@@ -92,41 +91,7 @@ public class BDD {
 		}
 		return 0;
 	}
-
-	/**
-	 * Récupère tous les utilisateurs de la BDD
-	 * 
-	 * @return utilisateurs
-	 */
-	public Set<Utilisateur> getAllUtilisateurs() {
-		return null;
-	}
-
-	public void setLogin(String value) {
-		login = value;
-	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setMotDePasse(String value) {
-		motDePasse = value;
-	}
-
-	public String getMotDePasse() {
-		return motDePasse;
-	}
-
-	public void setUrl(String value) {
-		url = value;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-	
-	
+		
 	public boolean existeUser(String id_utilisateur) {
 		ResultSet resultSet = null;
 		resultSet = requeteLecture("SELECT COUNT(id_utilisateur) AS total FROM utilisateur WHERE id_utilisateur='" + id_utilisateur + "'");
@@ -173,14 +138,18 @@ public class BDD {
 
 	public Map<Integer, String> getListFil(String id_utilisateur) {
 		Map<Integer, String> map = new HashMap<Integer, String>();
+		List<Integer> liste = new ArrayList<>();
 		ResultSet resultSet = null;
-		ResultSet resultSet2 = null;
 		resultSet = requeteLecture("SELECT id_filDiscussion FROM estdans WHERE id_utilisateur='" + id_utilisateur + "'");
 		try {
 			while (resultSet.next()) {
-				resultSet2 = requeteLecture("SELECT id_filDiscussion FROM fildiscussion WHERE id_filDiscussion='"+ resultSet.getInt("id_filDiscussion") + "'");
-				resultSet2.next();
-				map.put(resultSet.getInt("id_filDiscussion"), resultSet2.getString("premierMessage"));	
+				System.out.println(resultSet.getInt("id_filDiscussion"));
+				liste.add(resultSet.getInt("id_filDiscussion"));
+			}
+			for (Integer id_fil : liste) {
+				resultSet = requeteLecture("SELECT premierMessage FROM fildiscussion WHERE id_filDiscussion="+ id_fil);
+				resultSet.next();
+				map.put(id_fil, resultSet.getString("premierMessage"));	
 			}
 			return map;
 		} catch (SQLException e) {
@@ -190,19 +159,23 @@ public class BDD {
 	}
 
 	public FilDiscussion getFil(int id_filDiscussion) {
-		ResultSet resultSetFirst = null;
 		ResultSet resultSet = null;
-		resultSetFirst = requeteLecture("SELECT * FROM message WHERE id_filDiscussion = " + id_filDiscussion+ " ORDER BY date_emission LIMIT 1");
-		Message message = null;
-		FilDiscussion fil = null;
+		resultSet = requeteLecture("SELECT * FROM message WHERE id_filDiscussion = " + id_filDiscussion+ " ORDER BY date_emission LIMIT 1");
+		Message message;
+		FilDiscussion fil;
 		try {
-			resultSetFirst.next();
-			resultSet = requeteLecture("SELECT * FROM utilisateur WHERE id_utilisateur='" + resultSetFirst.getString("id_utilisateur") + "'");
 			resultSet.next();
-			message = new Message(resultSetFirst.getString("id_utilisateur"), resultSet.getString("nom"),
-					resultSet.getString("prenom"), resultSetFirst.getString("date_emission"),
-					resultSetFirst.getString("statut"), resultSetFirst.getString("contenue"));
-			fil = new FilDiscussion(message, id_filDiscussion, resultSetFirst.getInt("id_groupe"));
+			String id = resultSet.getString("id_utilisateur");
+			String date = resultSet.getString("date_emission");
+			String statut = resultSet.getString("statut");
+			String contenu = resultSet.getString("contenu");
+			resultSet = requeteLecture("SELECT * FROM utilisateur WHERE id_utilisateur='" + id + "'");
+			resultSet.next();
+			message = new Message(id, resultSet.getString("nom"), resultSet.getString("prenom"), date, statut, contenu);
+			resultSet = requeteLecture("SELECT id_groupe FROM filDiscussion WHERE id_filDiscussion=" + id_filDiscussion);
+			resultSet.next();
+			System.out.println(message.getDate());
+			fil = new FilDiscussion(message, id_filDiscussion, resultSet.getInt("id_groupe"));
 			return fil;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -276,22 +249,8 @@ public class BDD {
 				+ id_utilisateur + "'," + id_fil + ")");
 	}
 
-//	INSERT INTO fildiscussion VALUES (3, 1, 'Mon message');
-//	INSERT INTO message VALUES (NULL, '2021-12-31 12:35:59', 'Rouge','Mon message','Dpt01',3)
 
-	/*-- existeUser(String Id_user) : boolean
-	 *
-	 *-- getAllUser() : List user
-	 *-- getHash(String id_user) : String 32 
-	 *-- getListFil(String id_user) : Map<int><String> 
-	 *-- getFil(ID_fil) : Fil fil
-	 *-- getUtilisateur(String Id_user) : User user
-	 *-- getListGroupe() : Map<int><String> 
-	 *-- getGroupe(int id_groupe) String groupe
-	 * 
-	 *-- ajouterFil(String Id_user, Date date, String message, int Id_groupe) : Fil fil
-	 *-- ajouterMessage(String id_user,Int id_fil, Date date, String message) : int (0 true, 1 error)
-	 * ajouterUser(String id_user, String hashMDP, String Nom, String prenom) : int (0 true, 1 error, 2 existe deja)
+	/* ajouterUser(String id_user, String hashMDP, String Nom, String prenom) : int (0 true, 1 error, 2 existe deja)
 	 * ajouterGroupe(String role) : int (0 true, 1 error)
 	 * 
 	 * supUser(String ID_user) : (0 true, 1 error)
@@ -300,8 +259,6 @@ public class BDD {
 	 * supFil(Int ID_fil) :(0 true, 1 error)
 	 * 
 	 * inserGroupe(String Id_user, Int ID_groupe) : (0 true, 1 error)
-	 * 
-	 * SELECT * FROM `message` WHERE id_filDiscussion = 1 ORDER BY date_emission LIMIT 1
 	 */
 
 }
