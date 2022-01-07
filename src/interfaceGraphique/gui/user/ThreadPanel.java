959319dfb08154611dd95f995bf1a558cfc8970f
Kazragore <32024540+Menudelux95@users.noreply.github.com>
2022-01-07 04:39:29 +0100

    Add files via upload

--- src/interfaceGraphique/gui/user/ThreadPanel.java
+++ src/interfaceGraphique/gui/user/ThreadPanel.java
@@ -10,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.ListIterator;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -30,6 +31,7 @@ public class ThreadPanel extends JPanel {
 	private static final long serialVersionUID = 8327177803774656189L;
 	private JTextArea messageTextArea;
 	private FilDiscussion fd;
+	JScrollPane scrollPane_2;
 	
 	public ThreadPanel(DefaultMutableTreeNode node, Utilisateur currentUser) {
 		this.setBackground(Color.DARK_GRAY);
@@ -57,20 +59,25 @@ public class ThreadPanel extends JPanel {
 		
 			messageTextArea = new JTextArea();
 			scrollPane.setViewportView(messageTextArea);
+		
 		//panel affichant les sujets
 		fd = (FilDiscussion)node.getUserObject();
 		
 		JPanel panel_2 = new JPanel();
-		this.add(panel_2, BorderLayout.CENTER);
 		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+		panel_2.setBorder(BorderFactory.createLineBorder(Color.red));
+		panel_2.setPreferredSize(new Dimension(0, 1500));
+		scrollPane_2 = new JScrollPane(panel_2);
+		scrollPane_2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		this.add(scrollPane_2, BorderLayout.CENTER);
+	
 		List<Message> lm = currentUser.getFilDiscussion(fd.getId_filDiscussion()).getMessages();
 		for(ListIterator<Message> iterareur = lm.listIterator(); iterareur.hasNext();) {	
 			Message message = iterareur.next();
 
 			JPanel panel_5 = new JPanel();
 			panel_5.setBackground(Color.DARK_GRAY);
-			panel_5.setPreferredSize(new Dimension(480, 150));
+			panel_5.setPreferredSize(new Dimension(460, 140));
 			panel_2.add(panel_5);
 			panel_5.setLayout(new BorderLayout(0, 0));
 		
@@ -91,6 +98,43 @@ public class ThreadPanel extends JPanel {
 		}
 	}
 	
+	public void actualiserFil() {
+		JPanel panel_2 = new JPanel();
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel_2.setBorder(BorderFactory.createLineBorder(Color.red));
+		panel_2.setPreferredSize(new Dimension(0, 1500));
+		this.remove(scrollPane_2);
+		this.revalidate();
+		scrollPane_2 = new JScrollPane(panel_2);
+		scrollPane_2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		this.add(scrollPane_2, BorderLayout.CENTER);
+		
+		List<Message> lm = User.getCurrentUser().getFilDiscussion(fd.getId_filDiscussion()).getMessages();
+		for(ListIterator<Message> iterareur = lm.listIterator(); iterareur.hasNext();) {	
+			Message message = iterareur.next();
+
+			JPanel panel_5 = new JPanel();
+			panel_5.setBackground(Color.DARK_GRAY);
+			panel_5.setPreferredSize(new Dimension(460, 140));
+			panel_2.add(panel_5);
+			panel_5.setLayout(new BorderLayout(0, 0));
+		
+			JScrollPane scrollPane_1 = new JScrollPane();
+			scrollPane_1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			panel_5.add(scrollPane_1, BorderLayout.CENTER);
+		
+			JTextArea txtrLoremIpsumDolor = new JTextArea();
+			txtrLoremIpsumDolor.setBackground(selectionnerCouleur(message.getStatut()));
+			txtrLoremIpsumDolor.setLineWrap(true);
+			txtrLoremIpsumDolor.setText(message.getMessage());
+			txtrLoremIpsumDolor.setEditable(false);
+			scrollPane_1.setViewportView(txtrLoremIpsumDolor);
+		
+			JLabel lblNewLabel = new JLabel("Par " + message.getPrenom() + " " + message.getNom() + " le " + message.getDate());
+			lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			panel_5.add(lblNewLabel, BorderLayout.NORTH);
+		}
+	}
 	private Color selectionnerCouleur(String couleur) {
 		switch(couleur) {
 		case "Rouge": return Color.RED;
@@ -105,5 +149,6 @@ public class ThreadPanel extends JPanel {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		message = new Message(User.getCurrentUser().getIdentifiant(), User.getCurrentUser().getNom(), User.getCurrentUser().getPrenom(), dtf.format(LocalDateTime.now()), "Rouge", messageTextArea.getText());
 		User.getCurrentUser().envoyerMessage(message, fd);
+		actualiserFil();
 	}
 }
