127333c888d5302382474fcea5725a4c6786cbbc
Bastini <33761095+Bastini288@users.noreply.github.com>
2022-01-07 18:09:50 +0100

    Update FilDiscussion.java

--- src/utilisateur/FilDiscussion.java
+++ src/utilisateur/FilDiscussion.java
@@ -8,11 +8,19 @@ public class FilDiscussion {
 	private List<Message> messages = new ArrayList<>();
 	private int id_filDiscussion;
 	private String id_groupe;
-
+	private int nb_messageNonLu;
+	
 	public FilDiscussion(Message ticket,int id_filDiscussion, String id_groupe) {
 		this.messages.add(ticket);
 		this.id_filDiscussion = id_filDiscussion;
 		this.id_groupe = id_groupe;
+	}	
+	
+	public FilDiscussion(Message ticket,int id_filDiscussion, String id_groupe, int nb_messageNonLu) {
+		this.messages.add(ticket);
+		this.id_filDiscussion = id_filDiscussion;
+		this.id_groupe = id_groupe;
+		this.nb_messageNonLu = nb_messageNonLu;
 	}	
 	
 	public List<Message> getMessages() {
@@ -34,4 +42,14 @@ public class FilDiscussion {
 	public String toString() {
 		return messages.get(0).getMessage();
 	}
+
+	public int getNb_messageNonLu() {
+		return nb_messageNonLu;
+	}
+
+	public void setNb_messageNonLu(int nb_messageNonLu) {
+		this.nb_messageNonLu = nb_messageNonLu;
+	}
+	
+	
 }
